8b6ebf86dc12ef80bf9998f2864f441e8461958f
KariRosenova <karii_6655@abv.bg>
2017-11-05 17:36:32 +0200

    Initial commit

---
+++ STD1A-KarolinaRosenova-1701681019-H6/GeometryCalculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        static double Triangle(double a, double ha)
+        {
+            return a * ha;
+        }
+
+        static double Square(double s)
+        {
+            return s * s;
+        }
+
+        static double Degrees(double d)
+        {
+           return  (Math.PI / 180) * d;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose option (triangle, square or radians):");
+            var choose = Console.ReadLine();
+
+            if (choose == "triangle")
+            {
+                double a, ha;
+                Console.WriteLine("Enter side: ");
+                a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter height: ");
+                ha = double.Parse(Console.ReadLine());
+                Console.WriteLine("The area of the triangle is = {0:F2}", (Triangle(a, ha)));               
+            }
+
+            else if (choose == "square")
+            {
+                double s;
+                Console.WriteLine("Enter side: ");
+                s = double.Parse(Console.ReadLine());
+                Console.WriteLine("The area of the square is = {0:F2}", (Square(s)));
+            }
+
+            else if (choose == "radians")
+            {
+                double d;
+                Console.WriteLine("Enter the degrees you want to transform in radians: ");
+                d = double.Parse(Console.ReadLine());
+                Console.WriteLine("Your degrees in radians are = {0:F2}", (Degrees(d)));
+            }
+
+            else
+            {
+                Console.WriteLine("Choose from shown options!");
+            }
+        }
+    }
+}
---
+++ STD1A-KarolinaRosenova-1701681019-H6/GetMax/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMax
+{
+    class Program
+    {
+        static int a;
+        static void GetMax(int first, int second)
+        {
+            if (first > second) a = first;
+            else a = second;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number: ");
+            int c = Int32.Parse(Console.ReadLine());
+
+            GetMax(a, b);
+            GetMax(a, c);
+
+            Console.WriteLine("Biggest number is {0}", a);
+        }
+    }
+}
+
